a0216694f95a2a6f6365f0316bfeb3cacaa36d7a
stefano11111 <134295222+stefano11111@users.noreply.github.com>
2023-07-03 15:43:31 +0200

    Update ate.py

--- ate.py
+++ ate.py
@@ -1,5 +1,8 @@
 a="Laura and tom went ON a long walk."
-a=a.lower()
-a=a.capitalize()
-a.replace("tom","TOM")
+a=a.replace("ON","on")
+a=a.replace("tom","TOM")
+a=a.replace("long","LONG")
+a=a.replace("walk","wolk")
 print(a)
+b=a.split(" ")
+print(b)
