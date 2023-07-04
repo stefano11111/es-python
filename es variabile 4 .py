cbd22b6cb38ef0ea29c714f0ffaa8d9887754070
ste <stefanocapra29@gmail.com>
2023-07-04 17:37:47 +0200

    add

---
+++ es variabile 4 .py
@@ -0,0 +1,21 @@
+name=input("tell me your name ")
+age=input("tell me your age ")
+city=input("tell me your city ")
+residence=input("tell me your residence ")
+print(f"your name is {name}, your age is {age}, your city is {city} and your residence is {residence}")
+name.upper()
+city.lower()
+print(f"the length of name is {len(name)}")
+print(f"the length of age is {len(age)}")
+print(f"the length of city is {len(city)}")
+print(f"the length of residence is {len(residence)}")
+first_name="stefano"
+last_name="capra"
+full_name=first_name+" "+last_name
+fav_language="python"
+print(f"my favourite programming language is {fav_language}")
+current_year=2023
+birth_year=1995
+print(f"the age of the person is {current_year-birth_year}")
+sentence="this variable is a sentence"
+print(f"the variable sentence has {len(sentence)} characters")
