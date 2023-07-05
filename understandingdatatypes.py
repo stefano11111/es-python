342889cb7ca3e381d025cd9a8ad88a6606965d77
ste <stefanocapra29@gmail.com>
2023-07-05 23:30:20 +0200

    a

---
+++ understandingdatatypes.py
@@ -0,0 +1,14 @@
+# Create variables of different data types (integer, float, string, and boolean)
+# Use the type function to print the type of each variable
+# Observe how the data type affects the output of certain operations, 
+#for example: • Adding two integers • Concatenating a string with a float • Comparing a string with a boolean
+
+a=1
+b=1.5
+c="this is a string"
+d=True
+print(type(a), type(b), type(c), type(d))
+e=1+2
+#f=b+c
+g=(c==d)
+print(e,  g)
