ef720067a16175d59c1ccbeed1fc289f4a8b351d
ste <stefanocapra29@gmail.com>
2023-07-05 10:55:38 +0200

    a

---
+++ es operatore 4.py
@@ -0,0 +1,3 @@
+#print(not ("testing" == "testing" ... "Mario" == "Cool Guy")) # Should print True
+
+print(not ("testing" == "testing" and "Mario" == "Cool Guy"))
---
+++ es variabile 5.py
@@ -0,0 +1,8 @@
+user="stefano capra"
+sentence="user name is stefano, he is 28 years old"
+print(user.isdigit())
+print(user[0], user[-1])
+a=user.split()
+sentence=sentence.replace("stefano", "marco")
+b=sentence.split(" ")
+print(f"{b[0]} \n {b[1]} \n {b[2]} \n {b[3]} \n {b[4]} \n {b[5]} \n {b[6]} \n {b[7]} \n {b[8]}" )
