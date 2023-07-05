45726d3d19077e2c3821abacd8358d5da87e8f85
ste <stefanocapra29@gmail.com>
2023-07-05 23:33:17 +0200

    a

---
+++ casting.py
@@ -0,0 +1,9 @@
+# Create a float variable float_num
+# Use the int function to cast float_num to an integer and store the result in a new variable int_num
+# Use the str function to cast int_num to a string and store the result in a new variable str_num
+# Print the original float_num and the result of each casting to observe the changes in the data type.
+
+float_num=1.5
+int_num=int(float_num)
+str_num=str(int_num)
+print(float_num, int_num, str_num)
