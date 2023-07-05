c7294584e2f1df4d07f9fc45ce0c682da1653426
ste <stefanocapra29@gmail.com>
2023-07-05 16:32:31 +0200

    a

--- es variabile 5.py
+++ es variabile 5.py
@@ -1,7 +1,7 @@
 user="stefano capra"
 sentence="user name is stefano, he is 28 years old"
-print("0" in user or "1" in user or "2" in user or "3" in user or "4" in user or "5" in user or "6" in user or "7" in user or "8" in user or "9" in user)
 print(user[0], user[-1])
+print("0" in user or "1" in user or "2" in user or "3" in user or "4" in user or "5" in user or "6" in user or "7" in user or "8" in user or "9" in user)
 a=user.split()
 sentence=sentence.replace("stefano", "marco")
 b=sentence.split(" ")
