1e1f3f4f403e8918e7d2177c21891edfd8c39aa8
ste <stefanocapra29@gmail.com>
2023-07-12 17:44:43 +0200

    a
    git commit -ma
    
    git add .

--- es operatori 10.py
+++ es operatori 10.py
@@ -2,8 +2,12 @@
 # Given a list numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], create and print a new list that contains only the odd numbers.
 # Given a list mixed = [1, "apple", 3, "banana", 5, "cherry"], create and print two separate lists, one containing the numbers and one containing the strings.
 sentence = "The quick brown fox jumps over the lazy dog."
-vowels=sentence.count("a")+sentence.count("e")+sentence.count("i")+sentence.count("o")+sentence.count("u")
-print(vowels)
+num_vowels=0
+vowels=["a", "e", "i", "o", "u"]
+for x in list(sentence):
+    if x in vowels:
+        num_vowels +=1
+print(num_vowels)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 list=[]
 for x in numbers:
