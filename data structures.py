cba203b49f0b31f00e76c62387e91d56a385b93c
ste <stefanocapra29@gmail.com>
2023-07-18 22:51:09 +0200

    a

---
+++ data structures.py
@@ -0,0 +1,13 @@
+list1 = ["lion", "monkey", "dog","fish"]
+tuple1 = ("lion", "monkey", "dog","fish")
+set1 = {"lion", "monkey", "dog","fish"}
+dict1 = {"lion":4, "monkey":2, "dog":4,"fish":2}
+print(len(list1), len(tuple1), len(set1), len(dict1))
+print(list1[0], tuple1[0])
+print(dict1["lion"])
+list1[2]="rabbit"
+#tuple1[2]="rabbit" le tuple non possono essere modificate
+list1.append("monkey")
+list1.remove("rabbit")
+dict1["fish"]=0
+print(list1,dict1)
